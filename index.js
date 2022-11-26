3d76547e8ae5a64f8159523e013bb73938176aaf
mostakshaikh50 <mostak.50.cse@gmail.com>
2022-11-26 04:20:09 +0000

    product details

--- index.js
+++ index.js
@@ -16,9 +16,26 @@ app.use(express.json());
 
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.s3sxdks.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 async function run(){
     try{
+        const categoryCollection = client.db('laptopDeals').collection('categories');
 
+        app.get('/categories', async(req, res) =>{
+            const query ={}
+            const cursor = categoryCollection.find(query);
+            const categories = await cursor.toArray();
+            res.send(categories);
+           });
+
+           app.get('/categories/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const category = await categoryCollection.findOne(query);
+            res.send(category);
+           });
     }
     finally{
 
